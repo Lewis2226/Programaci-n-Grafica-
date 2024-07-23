46a10d2f1cf369858e4b144c20accb8e75c9c130
Lewis2226 <131185164+Lewis2226@users.noreply.github.com>
2024-07-23 15:13:00 -0600

    ya quedo la poción, solo falta el dado

--- Assets/Scripts/Dado12.cs
+++ Assets/Scripts/Dado12.cs
@@ -5,62 +5,97 @@ using System.Collections.Generic;
 [RequireComponent(typeof(MeshRenderer))]
 public class Dado12 : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         CreateDodecahedron();
     }
-
     private void CreateDodecahedron()
     {
         Vector3[] vertices ={
+            new Vector3 (-1,0, 0), // vertice 0
             new Vector3 (1,0, 0), // vertice 1
-            new Vector3 (0,0, 0), // vertice 1
-            new Vector3 (-1,0, 0), // vertice 2
-            new Vector3 (0,0, 1), // vertice 3
-            new Vector3 (0,0, -1), // vertice 4
-            new Vector3 (0,0, 0), // vertice 5
-            new Vector3 (0,0, 0), // vertice 6
-            new Vector3 (0,0, 0), // vertice 7
-            new Vector3 (0,0, 0), // vertice 8
-            new Vector3 (0,0, 0), // vertice 9
-            new Vector3 (0,0, 0), // vertice 10
-            new Vector3 (0,0, 0), // vertice 11
-            new Vector3 (0,0, 0), // vertice 12
-            new Vector3 (0,0, 0), // vertice 13
-            new Vector3 (0,0, 0), // vertice 14
-            new Vector3 (0,0, 0), // vertice 15
-            new Vector3 (0,0, 0), // vertice 16
-            new Vector3 (0,0, 0), // vertice 17
-            new Vector3 (0,0, 0), // vertice 18
-            new Vector3 (0,0, 0), // vertice 19
+            new Vector3 (2,0, 1.5f), // vertice 2
+            new Vector3 (-2,0, 1.5f), // vertice 3
+            new Vector3 (2,0, 2), // vertice 4
+            new Vector3 (-2,0.6f, 0), // vertice 5
+            new Vector3 (2,0.6f, 0), // vertice 6
+            new Vector3 (-3,0.6f, 2.5f), // vertice 7
+            new Vector3 (3,0.6f, 2.5f), // vertice 8
+            new Vector3 (3,0.6f, 3), // vertice 9
+            new Vector3 (-2,0.8f, 0), // vertice 10
+            new Vector3 (2,0.8f, 0), // vertice 11
+            new Vector3 (-3,0.8f, 2.5f), // vertice 12
+            new Vector3 (3,0.8f, 2.5f), // vertice 13
+            new Vector3 (3,0.8f, 3), // vertice 14
+            new Vector3 (-1,1.4f, 0), // vertice 15
+            new Vector3 (1,1.4f, 0), // vertice 16
+            new Vector3 (2,1.4f, 1.5f), // vertice 17
+            new Vector3 (-2,1.4f, 1.5f), // vertice 18
+            new Vector3 (2,1.4f, 2), // vertice 19
 
         };
         int[] triagles =
         {
-            0,2,4, //Cara 0
+              
+             //Cara 0
+             0,4,1,
+             1,2,4,
+             0,3,4,
             
              //Cara 1
+             0,0,0,
+             0,0,0,
+             0,0,0,
 
              //Cara 2
+             0,0,0,
+             0,0,0,
+             0,0,0,
 
              //Cara 3
+             0,0,0,
+             0,0,0,
+             0,0,0,
 
              //Cara 4
+             0,0,0,
+             0,0,0,
+             0,0,0,
 
              //Cara 5
+             0,0,0,
+             0,0,0,
+             0,0,0,
 
              //Cara 6
+             0,0,0,
+             0,0,0,
+             0,0,0,
 
              //Cara 7
+             0,0,0,
+             0,0,0,
+             0,0,0,
 
              //Cara 8
+             0,0,0,
+             0,0,0,
+             0,0,0,
 
              //Cara 9
+             0,0,0,
+             0,0,0,
+             0,0,0,
 
              //Cara 10
+             0,0,0,
+             0,0,0,
+             0,0,0,
 
              //Cara 11
+             15,18,19,
+             15,19,16,
+             16,17,19,
         };
 
         Mesh mesh = GetComponent<MeshFilter>().mesh;
@@ -70,6 +105,4 @@ public class Dado12 : MonoBehaviour
         mesh.Optimize();
         mesh.RecalculateNormals();
     }
-
-
 }

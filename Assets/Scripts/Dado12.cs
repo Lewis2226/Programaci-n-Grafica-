59e586f1fba7d49f597af81dd86956fa48f85d76
L <ivanrobleslazaro@gmail.com>
2024-07-24 08:43:46 -0600

    todo listo para empezar con el flauros y terminar el 1D12

--- Assets/Scripts/Dado12.cs
+++ Assets/Scripts/Dado12.cs
@@ -49,52 +49,52 @@ public class Dad012 : MonoBehaviour
            0,3,4,
            
 
-           //Cara 2 lado 0 a 1
+           //Cara 2 lado 0 a 1 lado sup
            13,0,5,
            13,1,0,
            13,6,1,
            
-           //Cara 3 lado 0 a 4
+           //Cara 3 lado 0 a 4 lado sup
            12,4,9,
            12,0,4,
            12,5,0,
            
-           //Cara 4 lado 1 a 2
+           //Cara 4 lado 1 a 2 lado sup
            14,1,6,
            14,2,1,
            14,7,2,
            
-           //Cara 5 lado 2 a 3
+           //Cara 5 lado 2 a 3 lado sup
            10,2,7,
            10,3,2,
            10,8,3,
            
-           //Cara 6 lado 3 a 4
+           //Cara 6 lado 3 a 4 lado sup
            11,3,8,
            11,4,3,
            11,9,4,
            
-           //Cara 7 lado 5 a 6
+           //Cara 7 lado 5 a 6 lado inf
            9,10,15,
-           9,10,6,
-           9,10,12,
+           9,15,16,
+           9,16,11,
            
-           //Cara 8 lado 5 a 9
-           5,12,13,
-           5,13,9,
-           9,13,14,
+           //Cara 8 lado 5 a 9 lado inf
+           10,11,16,
+           10,11,7,
+           10,11,14,
            
-           //Cara 9 lado 6 a 7
+           //Cara 9 lado 6 a 7 lado inf
            6,13,14,
            6,14,7,
            7,14,15,
            
-           //Cara 10 lado 7 a 8
+           //Cara 10 lado 7 a 8 lado inf
            7,15,16,
            7,16,8,
            8,16,17,
            
-           //Cara 11 lado 8 a 9
+           //Cara 11 lado 8 a 9 lado inf
            8,14,13,
            8,13,9,
            9,13,15,

246c36560cf3372b11f9e4343dd98a0f30f2c31c
Lewis2226 <131185164+Lewis2226@users.noreply.github.com>
2024-07-25 23:55:03 -0600

    Flauros 1: ya se pinta y se pueden rotar antes de crear las figuras
    Se pinta mal, tengo que ver bien eso y se puede rotar antes de darle start esto si cambia la forma en la que se crean

--- Assets/Flauros/FlaurosMesh.cs
+++ Assets/Flauros/FlaurosMesh.cs
@@ -1,51 +1,34 @@
 using UnityEngine;
-
+[RequireComponent(typeof(MeshFilter))]
+[RequireComponent(typeof(MeshRenderer))]
 public class FlaurosMesh : MonoBehaviour
 {
 
     void Start()
     {
-        float size = 1f;
+        
         Vector3[] vertices = {
-            new Vector3(0, size, 0),
-            new Vector3(0, 0, 0),
-            new Vector3(size, size, 0),
-            new Vector3(size, 0, 0),
+            new Vector3(1, 0, 0),
+            new Vector3(0, 0, 1),
+            new Vector3(-1, 0, 0),
+            new Vector3(0, 1, 0),
 
-            new Vector3(0, 0, size),
-            new Vector3(size, 0, size),
-            new Vector3(0, size, size),
-            new Vector3(size, size, size),
-
-            new Vector3(0, size, 0),
-            new Vector3(size, size, 0),
-
-            new Vector3(0, size, 0),
-            new Vector3(0, size, size),
-
-            new Vector3(size, size, 0),
-            new Vector3(size, size, size),
         };
 
         int[] triangles = {
-            0, 2, 1, // front
-			1, 2, 3,
-            4, 5, 6, // back
-			5, 7, 6,
-            6, 7, 8, //top
-			7, 9 ,8,
-            1, 3, 4, //bottom
-			3, 5, 4,
-            1, 11,10,// left
-			1, 4, 11,
-            3, 12, 5,//right
-			5, 12, 13
-
-
+            
+            //Abajo
+            0,1,3,
+            //Lado 1
+            0,2,1,
+            //Lado 2
+            1,3,2,
+            //Lado 3
+            2,3,0,
         };
 
 
-        Vector2[] uvs = {
+       /* Vector2[] uvs = {
             new Vector2(0, 0.66f),
             new Vector2(0.25f, 0.66f),
             new Vector2(0, 0.33f),
@@ -65,12 +48,13 @@ public class FlaurosMesh : MonoBehaviour
             new Vector2(0.25f, 0),
             new Vector2(0.5f, 0),
         };
+       */
 
         Mesh mesh = GetComponent<MeshFilter>().mesh;
         mesh.Clear();
         mesh.vertices = vertices;
         mesh.triangles = triangles;
-        mesh.uv = uvs;
+        //mesh.uv = uvs;
         mesh.Optimize();
         mesh.RecalculateNormals();
     }

e8ab6b5e84d74e9b7e283f0d505eb400d6991fb8
Lewis2226 <131185164+Lewis2226@users.noreply.github.com>
2024-07-26 18:56:00 -0600

    ya se dibuja la figura del centro bien

--- Assets/Flauros/FlaurosMesh2.cs
+++ Assets/Flauros/FlaurosMesh2.cs
@@ -8,37 +8,49 @@ public class FlaurosMesh2 : MonoBehaviour
     {
        
         Vector3[] vertices = {
-          new Vector3(1, 0, 0), //0
-          new Vector3(0, 0, 1), //1
-          new Vector3(-1, 0, 0),//2
-          new Vector3(1, 0, 0),//3
-          new Vector3(0, 1, 0), //4
-          new Vector3(0, -1, 0),//5
+          new Vector3 (1,0,0), //Vertice 0
+          new Vector3 (-1,0,0), //Vertice 1
+          new Vector3 (0,0,1), //Vertice 2
+          new Vector3 (0,0,-1), //Vertice 3
+          new Vector3 (0,1,0), //Vertice 4
+          new Vector3 (0,-1,0), //Vertice 5
           
 
         };
 
         int[] triangles = {
            
-            /*
+            
             //Cara 0 Lado sub
-            0,4,1,
+            0,4,2,
           
             
-            //Cara 1 Lado 1 sub
-            1,4,2,
+            //Cara 1 Lado  sub
+            2,4,1,
             
-            */
-            //Cara 2 Lado 2 sub
+            
+            //Cara 2 Lado  sub
             3,4,0,
 
-            /*
+            
             // Cara 3 Lado sub
-            2,4,5,
-            /*
-            //Cara 4 Lado 1 inf
-            0,5,4,
-            */
+            1,4,3,
+            
+            //Cara 4 Lado inf
+            2,5,0,
+          
+            
+            //Cara 5 Lado inf
+            1,5,2,
+            
+            
+            //Cara 2 Lado 2 inf
+            0,5,3,
+
+            
+            // Cara 3 Lado inf
+            3,5,1,
+
         };
 
         /*
